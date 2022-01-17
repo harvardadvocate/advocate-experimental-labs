cef3077e2d7d7d7d883fee8c1b7baa5049b71b58
Albert Zhang <albert.b.zhang@gmail.com>
2022-01-17 01:20:00 -0500

    gradient added, basic text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,140 +1,150 @@
 import * as React from "react"
+import styled, {keyframes} from 'styled-components'
+// import "@fontsource/inter"
+import "@fontsource/inter/600.css"
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/200.css"
+import "@fontsource/ibm-plex-mono"
+import gradient1 from '../images/gradient1.webm'
+import gradient2 from '../images/gradient2.webm'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const StyledDiv = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  //padding-left: 150px;
+  width: 100vw;
+  height: 100vh;
+`
+const StyledHeading1 = styled.h1`
+  //position: relative;
+  //margin-top: 0;
+  //margin-bottom: 64px;
+  //margin: auto;
+  margin-top: 0;
+  max-width: 320px;
+  color: white;
+  font-size: 45px;
+  font-weight: 400;
+  font-family: inter;
+`
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+const StyledParagraph = styled.p`
+  margin-top: 0;
+  max-width: 450px;
+  color: white;
+  font-size: 25px;
+  font-weight: 200;
+  font-family: inter;
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+`
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+const StyledVideo = styled.video`
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+  z-index: -1;
+  position: relative;
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+`
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+const StyledLink = styled.a`
+  //padding: 10px;
+  //padding-left: 30px;
+  //padding-right: 30px;
+  background-color: transparent;
+  //border: 2px white solid;
+  border-radius: 500px;
+  border: none;
+  color: white;
+  font-size: 20px;
+  font-weight: 600;
+  font-family: inter;
+  text-decoration: none;
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+  &:hover {
+    text-decoration: underline;
+    text-underline-offset: 3px;
+  }
+`
+const horizontalAnimation = keyframes`
+  0% {
+    transform: translate(100%, 0);
+  }
+  100% {
+    transform: translate(-100%, 0);
+  }
+`
+const HorizontalScrollText = styled.h2`
+  max-width: 450px;
+  color: white;
+  font-size: 25px;
+  font-weight: 200;
+  font-family: "IBM Plex Mono";
+  animation-name: ${horizontalAnimation};
+  animation-duration: 8s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+`
+
+const Footer = styled.h3`
+  position: absolute;
+  bottom: 5px;
+  color: white;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: inter;
+  //font-family: "IBM Plex Mono";
+`
 
 // markup
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        ADVOCATE EXPERIMENTAL LABS WEBSITE
-      </h1>
-    </main>
-  )
+    return (<div>
+            <style jsx global>{`
+              body {
+                margin: 0px;
+                padding: 0px;
+              }
+            `}</style>
+
+            <StyledDiv>
+                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'flex-start'}}>
+                    <StyledHeading1>
+                        ADVOCATE
+                        <br/>
+                        EXPERIMENTAL
+                        <br/>
+                        LABS
+                    </StyledHeading1>
+
+                    <div style={{width: 100}}/>
+
+                    <div>
+                        <StyledParagraph>
+                            We’re a new creative labs founded at The Harvard Advocate, and we’d love to work with you.
+                        </StyledParagraph>
+
+                        <StyledLink href="https://forms.gle/VC3EKCJmdcX5tofs5" target="_blank" rel="noreferrer noopener">
+                            START A PROJECT WITH US
+                        </StyledLink>
+
+                        {/*<HorizontalScrollText>*/}
+                        {/*    Augmented Reality | NFTs | Experimental Performance Spaces | Art Residency Program | Haunted Houses | Flea Markets | Third Wave Coffee | Botany  |*/}
+                        {/*</HorizontalScrollText>*/}
+                    </div>
+
+                </div>
+
+                <Footer>21 SOUTH ST CAMBRIDGE, MA 02138 | COPYRIGHT © THE HARVARD ADVOCATE 2022</Footer>
+            </StyledDiv>
+
+            <StyledVideo preload="auto" autoPlay muted loop>
+                <source src={gradient1} type="video/webm"/>
+            </StyledVideo>
+        </div>
+
+    )
 }
 
 export default IndexPage
